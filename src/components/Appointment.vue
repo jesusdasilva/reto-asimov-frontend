40de7a43379179cce0c4cebc4bd8922d12523807
JesusManuel <jesusdasilva@gmail.com>
2022-05-11 00:41:14 -0400

    add disabled day

--- src/components/Appointment.vue
+++ src/components/Appointment.vue
@@ -6,28 +6,47 @@ import VRegistration from "@/components/Registration.vue";
 import VSchedule from "@/components/Schedule.vue";
 import VToolbar from "@/components/Toolbar.vue";
 import VDone from "@/components/Done.vue";
-import { toolbarStore, userStore } from "@/store";
-import axios from "axios"
+import { toolbarStore, userStore, calendarStore } from "@/store";
+import axios from "axios";
+
+const URL_BASE = "http://localhost:5000";
+const API_V1_RESERVATION = "/api/v1/reservation";
+const API_V1_RESERVATION_DISABLED_DATES =
+  API_V1_RESERVATION + "/disabled-dates";
+const httpClient = axios.create({
+  baseURL: URL_BASE,
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
 
 onMounted(() => {
   // console.log(moment(state.userData.date).format("YYYY-MM-DD"));
-
 });
+
+async function setDesabledDayByMonth(_month) {
+  const params = { _month };
+  const { data: {data} } = await httpClient(API_V1_RESERVATION_DISABLED_DATES, {
+    params
+  });
+  data.forEach(e => {
+    console.log(e.r_date)
+    calendarStore.disabledDates[1].days.push(parseInt(moment(e.r_date).format('DD')));
+  });
+
+}
 
 function onDayClick(date) {
   setDataDate(date);
-  setDisabledDay(date)
-
+  setDisabledDay(date);
 }
 
 async function onTimeClick(time) {
-  
   userStore.setHour(time);
-  await saveReservation() && toolbarStore.step.next();
+  (await saveReservation()) && toolbarStore.step.next();
 }
 
 async function saveReservation() {
-
   const params = {
     rFirstName: userStore.firstName,
     rLastName: userStore.lastName,
@@ -35,9 +54,12 @@ async function saveReservation() {
     rPhone: userStore.phone,
     rDate: userStore.date,
     rHour: userStore.hour
-    }
-console.log(params)
-  return await axios.post("http://localhost:3000/api/v1/reservation/", params);
+  };
+  console.log(params);
+  return await httpClient.post(
+    "http://localhost:3000/api/v1/reservation/",
+    params
+  );
 }
 
 function changeShowForm() {
@@ -65,7 +87,8 @@ provide("state", {
   onDayClick,
   onTimeClick,
   changeShowForm,
-  onSubmit
+  onSubmit,
+  setDesabledDayByMonth
 });
 </script>
 
--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { inject } from "vue";
 import moment from "moment";
 import { onMounted } from "vue"
 import { toolbarStore, calendarStore, userStore } from "@/store"
@@ -6,15 +7,19 @@ import VDescription from "@/components/schedule/Description.vue"
 import VDay from "@/components/schedule/Day.vue";
 import VHour from "@/components/schedule/Hour.vue";
 
+const { setDesabledDayByMonth } = inject("state");
+
 onMounted(() => {
   toolbarStore.cancelButton.setLabel("Salir")
   toolbarStore.cancelButton.setShow(true) 
   toolbarStore.nextButton.setShow(false)
 
   calendarStore.setStart(new Date());
-  calendarStore.setDisabledDates([{ weekdays: [1, 7] }, { days: [] }]);
+  calendarStore.setDisabledDates([{ weekdays: [1, 7] }, { days: [25] }]);
 
   userStore.setDate(moment(new Date()).format("YYYY-MM-DD"))
+  setDesabledDayByMonth(moment(new Date()).format("MM"))
+console.log(calendarStore.disabledDates)
 });
 </script>
 
